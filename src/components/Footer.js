78dd3f8db674edae2ceb6d5b5b19c09cb25ea760
cmv303 <115678318+cmv303@users.noreply.github.com>
2023-05-04 17:32:12 -0400

    worked more on footer js and css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { LinkedIn } from "@mui/icons-material";
 import { GitHub } from "@mui/icons-material";
+import { Email } from "@mui/icons-material";
+import "../Footer.css"
 
 export default function Footer() {
   return (
     <footer className="footer">
-      <div>
-        <LinkedIn /> <GitHub />
+      <div className="socialMedia">
+        <LinkedIn /> <GitHub /> <Email />
       </div>
       <p> &copy; 2023 Camila Moreiras</p>
     </footer>
