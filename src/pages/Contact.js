39570a00afb30e09e115dd58093770cf88a7f327
cmv303 <115678318+cmv303@users.noreply.github.com>
2023-05-05 17:48:42 -0400

    stylized card container for contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,97 +1,117 @@
 import React from "react";
 import { useForm, useFormState } from "react-hook-form";
+import { Snackbar, IconButton, Card, CardContent, Typography, TextField, Button } from "@mui/material";
+import { Close } from "@mui/icons-material";
 import "../styles/Contact.css";
 
 export default function Contact() {
   const { register, handleSubmit, control } = useForm({
     defaultValues: {
-      firstName: "",
+      firstName: " ",
+      lastName: " ",
+      email: " ",
+      subject: " ",
+      message: " ",
     },
   });
   const { dirtyFields } = useFormState({
-    control,
+    control
   });
-  const onSubmit = (data) => console.log(data);
+  const [open, setOpen] = React.useState(false);
+  const onSubmit = (data) => {
+    console.log(data); 
+    setOpen(true)
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <div className="section" id="contactPage">
-      <h1>Contact Page</h1>
+      <h1>Contact Form</h1>
+  <Card variant="=outlined" className="form-container">
+    <CardContent>
+      <Typography gutterBottom variant="h5" component="section">
+      </Typography>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="form-row">
-          <div className="form-group col-md-6">
-            <label htmlFor='"firstName'>First Name *</label>
-            <input
+      <TextField
               id="firstName"
               {...register("firstName", { required: true })}
-              type="text"
-              placeholder="First Name"
-              className={`form-control ${
-                dirtyFields.firstName ? "is-dirty" : ""
-              }`}
+              label="First Name *"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+              className={dirtyFields.firstName ? "is-dirty" : ""}
             />
-            {dirtyFields.firstName && <p>Field is dirty.</p>}
-          </div>
-
-          <div className="form-group col-md-6">
-            <label htmlFor="lastName">Last Name *</label>
-            <input
+            {dirtyFields.firstName}
+            <TextField
               id="lastName"
               {...register("lastName", { required: true })}
-              type="text"
-              placeholder="Last Name"
-              className={`form-control ${
-                dirtyFields.lastName ? "is-dirty" : ""
-              }`}
+              label="Last Name *"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+              className={dirtyFields.lastName ? "is-dirty" : ""}
             />
-            {dirtyFields.lastName && <p>Field is dirty.</p>}
-          </div>
-        </div>
+            {dirtyFields.lastName}
+            <TextField
+              id="email"
+              {...register("email", {
+                required: true,
+                pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+              })}
+              label="Email Address *"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+              className={dirtyFields.email ? "is-dirty" : ""}
+            />
+            {dirtyFields.email}
+            <TextField
+              id="subject"
+              {...register("subject", { required: true })}
+              label="Subject *"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+              className={dirtyFields.subject ? "is-dirty" : ""}
+            />
+            {dirtyFields.subject}
+            <TextField
+              id="message"
+              {...register("message", { required: true })}
+              label="Message *"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+              multiline
+              rows={5}
+              className={dirtyFields.message ? "is-dirty" : ""}
+            />
+            {dirtyFields.message}
+            <Button type="submit" variant="contained" color="primary">
+              Send Message
+            </Button>
+          </form>
+    </CardContent>
+  </Card>
 
-        <div className="form-group">
-          <label htmlFor="email">Email Address *</label>
-          <input
-            id="email"
-            {...register("email", {
-              required: true,
-              pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-            })}
-            type="email"
-            placeholder="Email Address"
-            className={`form-control ${dirtyFields.email ? 'is-dirty' : ''}`}
-          />
-          {dirtyFields.email && <p>Field is dirty.</p>}
-        </div>
-        <br></br>
-        <br></br>
-
-        <div className="form-group">
-          <label htmlFor="subject">Subject *</label>
-          <input
-            id="subject"
-            {...register("subject", { required: true })}
-            type="text"
-            placeholder="Subject"
-            className={`form-control ${dirtyFields.subject ? 'is-dirty' : ''}`}
-          />
-          {dirtyFields.subject && <p>Field is dirty.</p>}
-        </div>
-        <br></br>
-        <br></br>
-
-        <div className="form-group">
-          <label htmlFor="message">Message *</label>
-          <textarea
-            id="message"
-            {...register("message", { required: true })}
-            rows={5}
-            placeholder="Enter your message"
-            className={`form-control ${dirtyFields.message ? 'is-dirty' : ''}`}
-          />
-          {dirtyFields.message && <p>Field is dirty.</p>}
-        </div>
-
-        <input type="submit" className="btn btn-primary" value="Send Message" />
-      </form>
+      <Snackbar
+        open={open}
+        autoHideDuration={3000}
+        onClose={handleClose}
+        message="Message sent"
+        action={
+          <IconButton
+          size="small"
+          aria-label="close"
+          color="inherit"
+          onClick={handleClose}
+          >
+            <Close fontSize="small" />
+          </IconButton>
+        }
+      />
     </div>
   );
 }
