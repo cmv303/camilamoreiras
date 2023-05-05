2255585a65ea22ab3b40619e74c998aa024a5957
cmv303 <115678318+cmv303@users.noreply.github.com>
2023-05-04 20:42:46 -0400

    worked on contact form

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 import { LinkedIn } from "@mui/icons-material";
 import { GitHub } from "@mui/icons-material";
 import { Email } from "@mui/icons-material";
-import "../Footer.css"
+import "../styles/Footer.css"
 
 export default function Footer() {
   return (
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,24 +1,96 @@
-import React from 'react';
+import React from "react";
 import { useForm, useFormState } from "react-hook-form";
+import "../styles/Contact.css";
 
 export default function Contact() {
   const { register, handleSubmit, control } = useForm({
     defaultValues: {
-      firstName: ""
-    }
+      firstName: "",
+    },
   });
   const { dirtyFields } = useFormState({
-    control
+    control,
   });
   const onSubmit = (data) => console.log(data);
 
   return (
-    <div className='section' id='contactPage'>
+    <div className="section" id="contactPage">
       <h1>Contact Page</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <input {...register("firstName")} placeholder="First Name" />
-        {dirtyFields.firstName && <p>Field is dirty.</p>}
-        <input type="submit" />
+        <div className="form-row">
+          <div className="form-group col-md-6">
+            <label htmlFor='"firstName'>First Name *</label>
+            <input
+              id="firstName"
+              {...register("firstName", { required: true })}
+              type="text"
+              placeholder="First Name"
+              className={`form-control ${
+                dirtyFields.firstName ? "is-dirty" : ""
+              }`}
+            />
+            {dirtyFields.firstName && <p>Field is dirty.</p>}
+          </div>
+
+          <div className="form-group col-md-6">
+            <label htmlFor="lastName">Last Name *</label>
+            <input
+              id="lastName"
+              {...register("lastName", { required: true })}
+              type="text"
+              placeholder="Last Name"
+              className={`form-control ${
+                dirtyFields.lastName ? "is-dirty" : ""
+              }`}
+            />
+            {dirtyFields.lastName && <p>Field is dirty.</p>}
+          </div>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="email">Email Address *</label>
+          <input
+            id="email"
+            {...register("email", {
+              required: true,
+              pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+            })}
+            type="email"
+            placeholder="Email Address"
+            className={`form-control ${dirtyFields.email ? 'is-dirty' : ''}`}
+          />
+          {dirtyFields.email && <p>Field is dirty.</p>}
+        </div>
+        <br></br>
+        <br></br>
+
+        <div className="form-group">
+          <label htmlFor="subject">Subject *</label>
+          <input
+            id="subject"
+            {...register("subject", { required: true })}
+            type="text"
+            placeholder="Subject"
+            className={`form-control ${dirtyFields.subject ? 'is-dirty' : ''}`}
+          />
+          {dirtyFields.subject && <p>Field is dirty.</p>}
+        </div>
+        <br></br>
+        <br></br>
+
+        <div className="form-group">
+          <label htmlFor="message">Message *</label>
+          <textarea
+            id="message"
+            {...register("message", { required: true })}
+            rows={5}
+            placeholder="Enter your message"
+            className={`form-control ${dirtyFields.message ? 'is-dirty' : ''}`}
+          />
+          {dirtyFields.message && <p>Field is dirty.</p>}
+        </div>
+
+        <input type="submit" className="btn btn-primary" value="Send Message" />
       </form>
     </div>
   );
