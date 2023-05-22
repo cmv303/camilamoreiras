6fe6e525cdc2a1c6aab65c79f06cd2ba0011a4c9
cmv303 <115678318+cmv303@users.noreply.github.com>
2023-05-22 13:09:46 -0400

    I think mui is overriding css styling for the header. need to go back to this bug later to incorporate similar styling as footer

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
+import "../styles/Header.css"
 
 export default function Header() {
-  return <div>Header</div>;
+  return <div className="header">Header</div>;
 }
--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -25,7 +25,7 @@ function NavTabs({ currentPage, handlePageChange }) {
     <AppBar
       className="nav-tabs"
       position="static"
-      sx={{ backgroundColor: "#2A9D8F" }}
+      // sx={{ backgroundColor: "#2A9D8F" }}
     >
       <div className="toggleButton" id={expandNavTabs ? "open" : "close"}>
         <button
