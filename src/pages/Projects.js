1a80eb00d12d1b74ad8d534d608be44f1c9b8834
cmv303 <115678318+cmv303@users.noreply.github.com>
2023-06-29 18:34:41 -0400

    getting closer to finalizing button styling on projects page, but still can't get them to align at all the same level

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -12,6 +12,7 @@ import lyrically from "../Assets/lyrically.png";
 import portfolio from "../Assets/portfolio1.png";
 import employeeTracker from "../Assets/employee_tracker.png";
 import dailyClawsmic from "../Assets/Daily_Clawsmic.png";
+import "../styles/Projects.css";
 
 export default function Projects() {
   return (
@@ -51,36 +52,34 @@ export default function Projects() {
                 <br />
                 Contributed primarily to the CSS and readme.
               </Typography>
-              <Button
-                variant="contained"
-                href="https://github.com/cmv303/Clawesome-Crew"
-                target="_blank"
-                rel="noreferrer noopener"
-                sx={{
-                  backgroundColor: "#282c34",
-                  color: "#f5f5f5",
-                  marginRight: "15%",
-                  marginTop: "5%"
-                }}
-              >
-                Repository
-              </Button>
-
-              <Button
-                variant="contained"
-                href="https://cosmic-gallary.herokuapp.com/"
-                target="_blank"
-                rel="noreferrer noopener"
-                sx={{
-                  backgroundColor: "#282c34",
-                  color: "#f5f5f5",
-                  marginRight: "15%",
-                  marginTop: "5%"
-                }}
-              >
-                Site
-              </Button>
             </CardContent>
+            <div className="project-button-wrapper">
+                <Button
+                  variant="contained"
+                  href="https://github.com/cmv303/Clawesome-Crew"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  sx={{
+                    backgroundColor: "#282c34",
+                    color: "#f5f5f5",
+                  }}
+                >
+                  Repository
+                </Button>
+
+                <Button
+                  variant="contained"
+                  href="https://cosmic-gallary.herokuapp.com/"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  sx={{
+                    backgroundColor: "#282c34",
+                    color: "#f5f5f5",
+                  }}
+                >
+                  Site
+                </Button>
+              </div>
           </Card>
         </Grid>
 
@@ -90,19 +89,19 @@ export default function Projects() {
               <Typography variant="h5" component="div">
                 Lyrically
               </Typography>
-                <CardMedia
-                  component="img"
-                  image={lyrically}
-                  height="194"
-                  title="Lyrically"
-                  sx={{
-                    borderRadius: "3px",
-                    width: "100%",
-                    height: "100%",
-                    paddingTop: "10px",
-                    paddingBottom: "10px",
-                  }}
-                />
+              <CardMedia
+                component="img"
+                image={lyrically}
+                height="194"
+                title="Lyrically"
+                sx={{
+                  borderRadius: "3px",
+                  width: "100%",
+                  height: "100%",
+                  paddingTop: "10px",
+                  paddingBottom: "10px",
+                }}
+              />
               <Typography variant="body2" color="text.secondary">
                 Full-stack group project uses two third-party APIs to access the
                 track names, lyrics, and videos of your favorite songs.
@@ -110,6 +109,8 @@ export default function Projects() {
                 <br />
                 Contributed to the filters, readme, and html
               </Typography>
+            </CardContent>
+            <div className="project-button-wrapper">
               <Button
                 variant="contained"
                 href="https://github.com/cmv303/SuperGroupOne"
@@ -118,8 +119,6 @@ export default function Projects() {
                 sx={{
                   backgroundColor: "#282c34",
                   color: "#f5f5f5",
-                  marginRight: "15%",
-                  marginTop: "5%"
                 }}
               >
                 Repository
@@ -133,13 +132,11 @@ export default function Projects() {
                 sx={{
                   backgroundColor: "#282c34",
                   color: "#f5f5f5",
-                  marginRight: "15%",
-                  marginTop: "5%"
                 }}
               >
                 Site
               </Button>
-            </CardContent>
+            </div>
           </Card>
         </Grid>
 
@@ -149,19 +146,19 @@ export default function Projects() {
               <Typography variant="h5" component="div">
                 Employee Tracker
               </Typography>
-                <CardMedia
-                  component="img"
-                  image={employeeTracker}
-                  height="194"
-                  title="Employee Tracker"
-                  sx={{
-                    borderRadius: "3px",
-                    width: "100%",
-                    height: "100%",
-                    paddingTop: "10px",
-                    paddingBottom: "10px",
-                  }}
-                />
+              <CardMedia
+                component="img"
+                image={employeeTracker}
+                height="194"
+                title="Employee Tracker"
+                sx={{
+                  borderRadius: "3px",
+                  width: "100%",
+                  height: "100%",
+                  paddingTop: "10px",
+                  paddingBottom: "10px",
+                }}
+              />
               <Typography variant="body2" color="text.secondary">
                 Command-line application that allows users to view various
                 components of the employee_tracker database, such as
@@ -172,36 +169,34 @@ export default function Projects() {
                 <br />
                 Demo video available through link
               </Typography>
-              <Button
-                variant="contained"
-                href="https://github.com/cmv303/Employee_Tracker"
-                target="_blank"
-                rel="noreferrer noopener"
-                sx={{
-                  backgroundColor: "#282c34",
-                  color: "#f5f5f5",
-                  marginRight: "15%",
-                  marginTop: "5%"
-                }}
-              >
-                Repository
-              </Button>
-
-              <Button
-                variant="contained"
-                href="https://drive.google.com/file/d/1r_5NjOQ6hT6QMHWgTK0L4u6RhNuheZws/view"
-                target="_blank"
-                rel="noreferrer noopener"
-                sx={{
-                  backgroundColor: "#282c34",
-                  color: "#f5f5f5",
-                  marginRight: "15%",
-                  marginTop: "5%"
-                }}
-              >
-                Site
-              </Button>
             </CardContent>
+            <div className="project-button-wrapper">
+                <Button
+                  variant="contained"
+                  href="https://github.com/cmv303/Employee_Tracker"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  sx={{
+                    backgroundColor: "#282c34",
+                    color: "#f5f5f5",
+                  }}
+                >
+                  Repository
+                </Button>
+
+                <Button
+                  variant="contained"
+                  href="https://drive.google.com/file/d/1r_5NjOQ6hT6QMHWgTK0L4u6RhNuheZws/view"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  sx={{
+                    backgroundColor: "#282c34",
+                    color: "#f5f5f5",
+                  }}
+                >
+                  Site
+                </Button>
+              </div>
           </Card>
         </Grid>
 
@@ -211,55 +206,51 @@ export default function Projects() {
               <Typography variant="h5" component="div">
                 First Portfolio Project
               </Typography>
-                <CardMedia
-                  component="img"
-                  image={portfolio}
-                  height="194"
-                  title="firstPortfolio"
-                  sx={{
-                    borderRadius: "3px",
-                    width: "100%",
-                    height: "100%",
-                    paddingTop: "10px",
-                    paddingBottom: "10px",
-                  }}
-                />
+              <CardMedia
+                component="img"
+                image={portfolio}
+                height="194"
+                title="firstPortfolio"
+                sx={{
+                  borderRadius: "3px",
+                  width: "100%",
+                  height: "100%",
+                  paddingTop: "10px",
+                  paddingBottom: "10px",
+                }}
+              />
               <Typography variant="body2" color="text.secondary">
                 Static web application to show where I started in my coding
                 journey, November 2022.
               </Typography>
-              <Button
-                variant="contained"
-                href="https://github.com/cmv303/Camila-is-learning-code"
-                target="_blank"
-                rel="noreferrer noopener"
-                sx={{
-                  backgroundColor: "#282c34",
-                  color: "#f5f5f5",
-                  marginRight: "15%",
-                  marginTop: "5%"
-                }}
-              >
-                Repository
-              </Button>
-
-              <Button
-                variant="contained"
-                href="https://cmv303.github.io/Camila-is-learning-code/"
-                target="_blank"
-                rel="noreferrer noopener"
-                sx={{
-                  backgroundColor: "#282c34",
-                  color: "#f5f5f5",
-                  marginRight: "15%",
-                  marginTop: "5%"
-                }}
-              >
-                Site
-              </Button>
-
-
             </CardContent>
+            <div className="project-button-wrapper">
+                <Button
+                  variant="contained"
+                  href="https://github.com/cmv303/Camila-is-learning-code"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  sx={{
+                    backgroundColor: "#282c34",
+                    color: "#f5f5f5",
+                  }}
+                >
+                  Repository
+                </Button>
+
+                <Button
+                  variant="contained"
+                  href="https://cmv303.github.io/Camila-is-learning-code/"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  sx={{
+                    backgroundColor: "#282c34",
+                    color: "#f5f5f5",
+                  }}
+                >
+                  Site
+                </Button>
+              </div>
           </Card>
         </Grid>
       </Grid>
