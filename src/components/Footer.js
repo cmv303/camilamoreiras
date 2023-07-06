705ad8c25982805ed6a2d7f06895dfb45f4be64e
cmv303 <115678318+cmv303@users.noreply.github.com>
2023-07-06 17:28:39 -0400

    worked on getting favicon to work again and started tweaking footer icons in relation to them coming off the footer in smaller screens

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,44 +6,44 @@ import { Grid } from "@mui/material";
 export default function Footer() {
   return (
     <footer className="Footer">
-      <Grid
-        container
-        justifyContent="center"
-        alignItems="center"
-        spacing={2}
-        className="SocialMedia"
-      >
-        <Grid item xs={4} sm={6} md={12}>
-          <a
-            href="https://www.linkedin.com/in/camilamoreiras/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <LinkedIn sx={{ fontSize: 18 }} />
-          </a>
+        <Grid
+          container
+          justifyContent="center"
+          alignItems="center"
+          spacing={2}
+          className="SocialMedia"
+        >
+          <Grid item xs={12} sm={12} md={4}>
+            <a
+              href="https://www.linkedin.com/in/camilamoreiras/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <LinkedIn sx={{ fontSize: 14 }} />
+            </a>
 
-          <a href="https://github.com/cmv303" target="_blank" rel="noreferrer">
-            <GitHub sx={{ fontSize: 18 }} />
-          </a>
+            <a href="https://github.com/cmv303" target="_blank" rel="noreferrer">
+              <GitHub sx={{ fontSize: 14 }} />
+            </a>
 
-          <a href="mailto: moreiras.camila@gmail.com">
-            <Email sx={{ fontSize: 18 }} />
-          </a>
+            <a href="mailto: moreiras.camila@gmail.com">
+              <Email sx={{ fontSize: 14 }} />
+            </a>
 
-          <a
-            href="https://drive.google.com/file/d/1ZMQsyM34LhK9cOBds317fbK_JKvJWOco/view"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <Description sx={{ fontSize: 18 }} />
-          </a>
-          <p>
-            Site built using React with Material UI
-            <br></br>
-            &copy; 2023 Camila Moreiras
-          </p>
+            <a
+              href="https://drive.google.com/file/d/1ZMQsyM34LhK9cOBds317fbK_JKvJWOco/view"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Description sx={{ fontSize: 14 }} />
+            </a>
+            <p>
+              Site built using React with Material UI
+              <br></br>
+              &copy; 2023 Camila Moreiras
+            </p>
+          </Grid>
         </Grid>
-      </Grid>
     </footer>
   );
 }
