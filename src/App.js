8bfe9eadd8b5e85b14ce624c9e491f4f89e9f893
cmv303 <115678318+cmv303@users.noreply.github.com>
2023-07-28 18:24:28 -0400

    added theme page to override material UIs default dettings, but so far I have it commented it out as it's only in process

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+// import { ColorModeContext, useMode } from "./theme";
+// import { CssBaseline, ThemeProvider } from "@mui/material";
 import PortfolioContainer from "./components/PortfolioContainer.js";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./styles/App.css";
 
 function App() {
+  // const [theme, colorMode] = useMode();
   return (
-    <Router basename="/camilamoreiras">
-      <div className="App">
-        <PortfolioContainer />
-      </div>
-    </Router>
+    // <ColorModeContext.Provider value={colorMode}>
+    //   <ThemeProvider theme={theme}>
+    //     <CssBaseline />
+        <Router basename="/camilamoreiras">
+          <div className="App">
+            <PortfolioContainer />
+          </div>
+        </Router>
+    //   </ThemeProvider>
+    // </ColorModeContext.Provider>
   );
 }
 
